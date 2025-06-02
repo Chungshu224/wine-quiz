a79f4d6f098e64a9c2c8bef2a479cdcfacb2fa44
Chungshu224 <chungshu224@gmail.com>
2025-06-03 01:28:30 +0800

    Update select.js

--- select.js
+++ select.js
@@ -177,7 +177,9 @@ function handleStartButtonClick() {
   localStorage.setItem('selectedRegions', JSON.stringify(selected));
   window.location.href = 'quiz.html';
 }
-
+ console.log('Start button clicked');
+  // ... 你的選取判斷
+  window.location.href = 'quiz.html';
 // 綁定事件與初始化畫面
 document.getElementById('start-button').onclick = handleStartButtonClick;
 renderRegionUI();
