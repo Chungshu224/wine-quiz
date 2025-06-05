d8338231be861f90d9d2a1d7b0acef4109e9e26d
Chungshu224 <chungshu224@gmail.com>
2025-06-05 21:37:33 +0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,14 +1,16 @@
 // 設定（如需增減國家請於此調整）
 const SHEET_INDEX = {
+ const SHEET_INDEX = {
   italy: { id: "1dFJJuIBfIF5mnzAAG2poQKMKQKTVhEUDHuS1YX9RilA", label: "義大利", flag: "🇮🇹" },
-    portugal: { id: "18GCPNoDPXu9EcfPd0EmnpEJb0DsP7vQaoAdbGo9cMs4", label: "葡萄牙", flag: "PT" },
-    Austria: { id: "1qaxLc9-GMyHgPJCajU0Cs0-vrI4nRFsqn1Y3nvj_-m8", label: "奧地利", flag: "AT" },
-    new_zealand: { id: "1O6TqxxB0YSitH9ySe3pncEx_Zl_kVtcJd2zslBDKw5Y", label: "紐西蘭", flag: NZ" },
-    hungary: { id: "1Rcf_mH4p1F05MhitSEkcjMaIU2mKmiXi77ifbOjfC14", label: "匈牙利", flag: "HU },
-    USA: { id: "1ZsGBl0jHPhQu9nV3k5rJhJ4ZXkkrwSvA6qhY0RaTRxE", label: "美國", flag: "US" },
-germany: { id: "1M0yjlT-bXXFhrV-snoGEPw6WhyMSwSEf4O6f7K_zo9o", label: "德國", flag: "DE },
+  portugal: { id: "18GCPNoDPXu9EcfPd0EmnpEJb0DsP7vQaoAdbGo9cMs4", label: "葡萄牙", flag: "🇵🇹" },
+  austria: { id: "1qaxLc9-GMyHgPJCajU0Cs0-vrI4nRFsqn1Y3nvj_-m8", label: "奧地利", flag: "🇦🇹" },
+  new_zealand: { id: "1O6TqxxB0YSitH9ySe3pncEx_Zl_kVtcJd2zslBDKw5Y", label: "紐西蘭", flag: "🇳🇿" },
+  hungary: { id: "1Rcf_mH4p1F05MhitSEkcjMaIU2mKmiXi77ifbOjfC14", label: "匈牙利", flag: "🇭🇺" },
+  usa: { id: "1ZsGBl0jHPhQu9nV3k5rJhJ4ZXkkrwSvA6qhY0RaTRxE", label: "美國", flag: "🇺🇸" },
+  germany: { id: "1M0yjlT-bXXFhrV-snoGEPw6WhyMSwSEf4O6f7K_zo9o", label: "德國", flag: "🇩🇪" },
   france: { id: "1-8sav2Dl1pi4EfnqNQhpMR0I-TjZhbaIUE6mrC1QbpU", label: "法國", flag: "🇫🇷" },
   spain: { id: "1Zngq4LPi1E7edjopwvr7MS2dCRN1GW2rKuOetHPuhnY", label: "西班牙", flag: "🇪🇸" }
+
 };
 const API_KEY = "AIzaSyCn4cdaBpY2Fz4SXUMtpMhAN84YvOQACcQ";
 const QUIZ_COUNT = 10;
